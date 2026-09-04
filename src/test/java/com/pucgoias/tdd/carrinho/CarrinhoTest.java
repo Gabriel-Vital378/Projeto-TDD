f7fde049625e036814391d42e0583b16022ab647
Gabriel <gabriel@example.com>
2026-09-04 08:50:00 +0000

    red: teste item 2 (aumentar total ao adicionar item)

--- src/test/java/com/pucgoias/tdd/carrinho/CarrinhoTest.java
+++ src/test/java/com/pucgoias/tdd/carrinho/CarrinhoTest.java
@@ -11,4 +11,12 @@ class CarrinhoTest {
         Carrinho carrinho = new Carrinho();
         assertEquals(0.0, carrinho.calcularTotal());
     }
+
+    @Test
+    void deveAumentarTotalAoAdicionarItem() throws Exception {
+        Carrinho carrinho = new Carrinho();
+        Produto produto = new Produto("Caneta", 2.50, 10);
+        carrinho.adicionarItem(produto, 3);
+        assertEquals(7.50, carrinho.calcularTotal(), 0.0001);
+    }
 }
